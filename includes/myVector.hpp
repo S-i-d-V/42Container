4cd8f434662a5166b2a779d04522704d2d173a75
Ugo Theveny <utheveny42@gmail.com>
2021-12-08 02:19:50 +0100

    setting up the vector class

--- includes/myVector.hpp
+++ includes/myVector.hpp
@@ -1,33 +1,66 @@
 #ifndef MYVECTOR_HPP
 #define MYVECTOR_HPP
 
-template <class T, class A = std::allocator<T>>
-class myVector{
-    public:
-    typedef A allocator_type;
-    typedef typename A::value_type      value_type;
-    typedef typename A::reference       reference;
-    typedef typename A::const_reference const_reference;
-    typedef typename A::different_type  different_type;
-    typedef typename A::size_type       size_type;
+//#include <vector>
+#include <utility>
+#include <memory>
 
-    class iterator{
-        public:
-            typedef typename A::value_type      value_type;
-            typedef typename A::reference       reference;
-            typedef typename A::const_reference const_reference;
-            typedef typename A::different_type  different_type;
-            typedef typename A::size_type       size_type;
-    }
+//============//
+//namespace ft//
+//============//
+namespace ft{
 
-    class const_iterator{
-        public:
-            typedef typename A::value_type      value_type;
-            typedef typename A::reference       reference;
-            typedef typename A::const_reference const_reference;
-            typedef typename A::different_type  different_type;
-            typedef typename A::size_type       size_type;
-    }
-}
+	template <typename _Tp, typename _Allocator = std::allocator<_Tp>>
+	class vector{
+
+		//==============//
+		//Public members//
+		//==============//
+	    public:
+
+			//==================//
+			//Typedef / typename//
+			//==================//
+			//value_type is an alias to template T
+			typedef				_Tp														value_type;
+
+			//allocator_type is an alias to std::allocator
+			typedef 			_Allocator 												allocator_type;
+
+			//aliases of member of std::allocator as our members
+			typedef typename 	allocator_type::reference								reference;
+	    	typedef typename 	allocator_type::const_reference							const_reference;
+			typedef typename 	allocator_type::size_type								size_type;
+	    	typedef typename 	allocator_type::difference_type							difference_type;
+			typedef typename	allocator_type::pointer									pointer;
+			typedef typename	allocator_type::const_pointer							const_pointer;
+
+			//=======================//
+			//Constructors/Destructor//
+			//=======================//
+			//Default
+			explicit	vector(allocator_type const& alloc = allocator_type());
+			//Fill
+			explicit	vector(size_type n, value_type const& val = value_type(), allocator_type const& alloc = allocator_type());
+			//Range
+			template <class InputIterator>
+						vector(InputIterator first, InputIterator last, allocator_type const& alloc = allocator_type());
+			//Copy
+						vector(vector& const src);
+
+			//Destructor
+						~vector();
+
+		//===============//
+		//Private members//
+		//===============//
+		private:
+			size_type _n;
+			value_type const& _val;
+			allocator_type const& _alloc;
+
+	}; //end of vector class
+
+}//end of namespace my
 
 #endif

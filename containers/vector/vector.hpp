9547af19362573e6fe500ec3815987eabeaef494
Ugo Theveny <utheveny42@gmail.com>
2021-12-30 18:05:00 +0100

    stack and vector cleaner

--- containers/vector/vector.hpp
+++ containers/vector/vector.hpp
@@ -1,5 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/30 17:33:11 by user42            #+#    #+#             */
+/*   Updated: 2021/12/30 18:04:33 by user42           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//Protection of the header
 #ifndef VECTOR_HPP
 #define VECTOR_HPP
+
+/*****************************************************************************/
+/*                                Includes                                   */
+/*****************************************************************************/
 
 //Utils headers
 #include "../../utils/enable_if.hpp"
@@ -15,54 +32,46 @@
 #include <memory>
 #include <stdexcept>
 
-//Header temporaires
-#include <iostream>
-#include <limits>
-
-//============//
-//namespace ft//
-//============//
+/*****************************************************************************/
+/*                                Namespace FT                               */
+/*****************************************************************************/
 namespace ft{
-	//============//
-	//vector class//
-	//============//
+
+	/*****************************************************************************/
+	/*                                Class vector                               */
+	/*****************************************************************************/
 	template < typename T, class Alloc = std::allocator<T> >
 	class vector{
 
-		//==============//
-		//Public members//
-		//==============//
+		/*****************************************************************************/
+		/*                             Public members                                */
+		/*****************************************************************************/
 	    public:
 
-			//==================//
-			//Typedef / typename//
-			//==================//
+			/*****************************************************************************/
+			/*                              Members types                                */
+			/*****************************************************************************/
 
-			//value_type is the type that represent T
+			//Defining every types
 			typedef		T														value_type;
-
-			//allocator_type is the type that represent std::allocator
 			typedef 	Alloc 													allocator_type;
-
-			//define types of std::allocator as ours
 			typedef 	T&														reference;
 	    	typedef 	T const&												const_reference;
 			typedef 	T*														pointer;
 			typedef 	T const*												const_pointer;
-
 			typedef 	size_t													size_type;
 	    	typedef 	std::ptrdiff_t											difference_type;
 
-			//define iterators type as ours
+			//Defining every iterators
 			typedef		ft::vector_iterator<T>									iterator;
 			typedef		ft::vector_iterator<T const>							const_iterator;
 			typedef		ft::reverse_iterator<iterator>							reverse_iterator;
 			typedef		ft::reverse_iterator<const_iterator>					const_reverse_iterator;
 
 
-			//=======================//
-			//Constructors/Destructor//
-			//=======================//
+			/*****************************************************************************/
+			/*                               Constructors                                */
+			/*****************************************************************************/
 
 			//Default constructor (Build an empty container with no elements)
 			explicit	vector(allocator_type const& alloc = allocator_type()) : _alloc(alloc){
@@ -106,7 +115,10 @@ namespace ft{
 				return;
 			}
 
-			//Destructor
+			/*****************************************************************************/
+			/*                                Destructors                                */
+			/*****************************************************************************/
+			
 			~vector(){
 				for (int i = _size - 1; i >= 0; i--){
 					_alloc.destroy(&_data[i]);
@@ -117,9 +129,9 @@ namespace ft{
 				return;
 			}
 
-			//==================//
-			//Operator overloads//
-			//==================//
+			/*****************************************************************************/
+			/*                           Operators overloads                             */
+			/*****************************************************************************/
 
 			//Assignement operator
 			vector&		operator=(vector const& rhs){
@@ -136,9 +148,9 @@ namespace ft{
 			}
 			
 
-			//===========//
-			// Iterators //
-			//===========//
+			/*****************************************************************************/
+			/*                           Iterators functions                             */
+			/*****************************************************************************/
 
 			iterator				begin(){
 				return (iterator(_data));
@@ -156,9 +168,9 @@ namespace ft{
 				return (reverse_iterator(_data));
 			}
 
-			//==========//
-			// Capacity //
-			//==========//
+			/*****************************************************************************/
+			/*                            Capacity functions                             */
+			/*****************************************************************************/
 
 			//Return the number of elements of the container
 			size_type				size()		const{
@@ -182,7 +194,6 @@ namespace ft{
 				return (false);
 			}
 
-			//PEUT ETRE EXCEPTION SI N > MAXSIZE
 			//Resize the container so that it contains n elements;
 			void					resize(size_type n, value_type val = value_type()){
 				if (n < _size){
@@ -218,9 +229,9 @@ namespace ft{
 				}
 			}
 
-			//================//
-			// Element access //
-			//================//
+			/*****************************************************************************/
+			/*                         Elements access functions                         */
+			/*****************************************************************************/
 
 			//Return a reference to the elements at positon n in the container
 			reference				at(size_type n){
@@ -253,32 +264,26 @@ namespace ft{
 				return (_data[_size - 1]);
 			}
 
-			//==========//
-			// Modifier //
-			//==========//
+			/*****************************************************************************/
+			/*                            Modifiers functions                            */
+			/*****************************************************************************/
 
-			//Assign new contents to the vector by replace current content and modifying its size.
-				//By range of iterators
+			//Clear the content and assign a new content to the vector.
+			//1-Assign a new content from a range of iterators
 			template <class inputIterator>
 			void					assign(inputIterator first, inputIterator last){
-				//i clear the vector;
 				clear();
-				//i insert the range of iterator
 				insert(begin(), first, last);
 			}
 
-				//Fill
+			//1-Assign n times val as new content
 			void					assign(size_type n, value_type const& val){
-				//i clear the vector;
 				clear();
-				//i insert the range of iterator
 				insert(begin(), n, val);
 			}
 
-
 			//Add a new element at the end of the container and increases the storage space only if the new size surpasses the current capacity
 			void					push_back(value_type const& val){
-				//Pas besoin de reallocation
 				if (_size + 1 <= _capacity){
 					_alloc.construct(&_data[_size], val);
 					_size++;
@@ -300,124 +305,88 @@ namespace ft{
 				_size--;
 			}
 
-	
-
-			//Extend the container by inserting new element before the element at specified position and increase the size by the number of elements.
-				//Single element
+			//Extend the container by inserting new element before the element pointed by position and increase the size by the number of elements.
+			//1- Insert a single element
 			iterator				insert(iterator position, value_type const& val){
-				//get the index of the iterator in the vector
 				difference_type index = &(*position) - &(*begin());
-				//if the final size if bigger than capacity i realloc
 				if (_size + 1 > _capacity)
 					reserve(_capacity * 2);
-				//i set an iterator pointing on the index got from position.
 				iterator newPos(&_data[index]);
-				//if the iterator isnt at the end of the vector, i shift every thing to the right
 				if (newPos != end()){
-					//i shift to 1 place all elements from the end to ther new position end + 1.
 					for (iterator initialEnd(end()); initialEnd != newPos; initialEnd--)
 						_alloc.construct(&(*(initialEnd)), *(initialEnd - 1));
 				}
-				//i put the elements to insert
 				_alloc.construct(&(*newPos), val);
-				//i increase the size
 				_size += 1;
 			}
 			
-				//Fill
+			//2- Insert n times val in the vector from position
 			void					insert(iterator position, size_type n, value_type const& val){
-				//get the index of the iterator in the vector
 				difference_type index = &(*position) - &(*begin());
-				//if the final size if bigger than capacity i realloc
 				if (_size + n > _capacity && _size + n <= _capacity * 2)
 					reserve(_capacity * 2);
 				else if (_size + n > _capacity * 2)
 					reserve(_size + n);
-				//i set an iterator pointing on the index got from position.
 				iterator newPos(&_data[index]);
-				//if the iterator isnt at the end of the vector, i shift every thing to the right
 				if (newPos != end()){
-					//i shift to n place all elements from the end to ther new position end + n.
 					for (iterator initialEnd(end()); initialEnd != newPos; initialEnd--)
 						_alloc.construct(&(*(initialEnd + static_cast<int>(n) - 1)), *(initialEnd - 1));
 				}
-				//i put the elements to insert
 				for (size_type i = 0; i < static_cast<int>(n); i++, newPos++)
 					_alloc.construct(&(*newPos), val);
-				//i increase the size
 				_size += n;
 			}
 
-				//By range of iterators
+			//3- Insert a range of iterator in the vector from position
 			template <class inputIterator>
 			void					insert(iterator position, inputIterator first, inputIterator last,
 			typename ft::enable_if<!ft::is_integral<inputIterator>::value>::type* = 0){
-				//get the index of the iterator in the vector
 				difference_type index = &(*position) - &(*begin());
-				//get the size of the range first last
 				difference_type n = &(*last) - &(*first); 
-				//if the final size if bigger than capacity i realloc
 				if (_size + n > _capacity && _size + n <= _capacity * 2)
 					reserve(_capacity * 2);
 				else if (_size + n > _capacity * 2)
 					reserve(_size + n);
-				//i set an iterator pointing on the index got from position.
 				iterator newPos(&_data[index]);
-				//if the iterator isnt at the end of the vector, i shift every thing to the right
 				if (newPos != end()){
-					//i shift to n place all elements from the end to ther new position end + n.
 					for (iterator initialEnd(end()); initialEnd != newPos; initialEnd--)
 						_alloc.construct(&(*(initialEnd + static_cast<int>(n) - 1)), *(initialEnd - 1));
 				}
-				//i put the elements to insert
 				while (first != last){
 					_alloc.construct(&(*newPos), *first);
 					first++;
 					newPos++;
 				}
-				//i increase the size
 				_size += n;
 			}
 
-			//Remove 1 or a range of elements from the container and reduce the container size by the number of elements removed.
-				//Single element
+			//Erase 1 elements or a range of elements.
+			//1-Erase the element pointed by position on the vector 
 			iterator				erase(iterator position){
-				//get the index of the iterator in the vector
 				difference_type index = &(*position) - &(*begin());
-				//i set an iterator pointing on the index got from position.
 				iterator newPos(&_data[index]);
-				//if the iterator isnt at the end of the vector, i shift every thing to the left
 				if (newPos != end()){
-					//i shift to 1 place all elements from newpos to ther new position newpos + 1;
 					while (newPos != end()){
 						_alloc.construct(&(*(newPos)), *(newPos + 1));
 						newPos++;
 					}
 				}
-				//i destroy the last elements
 				_alloc.destroy(&(*end()));
-				//i decrease the size
 				_size -= 1;
 			}
 
-				//By range of iterators
+			//2-Erase every elements between first and last.
 			iterator				erase(iterator first, iterator last){
-				//get the index of the range in the vector
 				difference_type index = &(*first) - &(*begin());
-				//get the size of the range first last
 				difference_type n = &(*last) - &(*first); 
-				//i set an iterator pointing on the index got from position.
 				iterator newPos(&_data[index]);
-				//if the iterator isnt at the end of the vector, i shift every thing to the left
 				if (newPos != end()){
-					//i shift to 1 place all elements from newposto ther new position newPos + n.
 					while (newPos != last){
 						_alloc.destroy(&(*(newPos)));
 						_alloc.construct(&(*(newPos)), *(newPos + n));
 						newPos++;
 					}
 				}
-				//i destroy the n last elements and decreaze the size;
 				while (n > 0){
 					_alloc.destroy(&(*end()));
 					_size--;
@@ -436,18 +405,20 @@ namespace ft{
 				}
 			}
 
-			//===========//
-			// Allocator //
-			//===========//
-				//Return a copy of the allocator object associated with the vector;
+			/*****************************************************************************/
+			/*                            Allocator functions                            */
+			/*****************************************************************************/
+
+			//Return a copy of the allocator object associated with the vector;
 			allocator_type	get_allocator()		const{
 				return (_alloc);
 			}
 
 
-		//=================//
-		// Private members //
-		//=================//
+		/*****************************************************************************/
+		/*                            Private members                                */
+		/*****************************************************************************/
+
 		private:
 			size_type		_size;
 			size_type		_capacity;
@@ -456,46 +427,11 @@ namespace ft{
 
 	}; //end of vector class
 
-	//====================//
-	//Non-member overloads//
-	//====================//
+	/*****************************************************************************/
+	/*                           Non-members functions                           */
+	/*****************************************************************************/
 
-		//Comparaison operators
-	template <class T, class Alloc>
-	bool	operator==(vector<T, Alloc> const& lhs, vector<T, Alloc> const& rhs){
-		if (lhs.size() == rhs.size())
-			return (ft::equal(lhs.begin(), lhs.end(), rhs.begin()));
-		return (false);
-	}
-
-	template <class T, class Alloc>
-	bool	operator!=(vector<T, Alloc> const& lhs, vector<T, Alloc> const& rhs){
-		if (!(lhs == rhs))
-			return (true);
-		return (false);
-	}
-
-	template <class T, class Alloc>
-	bool	operator<(vector<T, Alloc> const& lhs, vector<T, Alloc> const& rhs){
-		return (ft::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end()));
-	}
-
-	template <class T, class Alloc>
-	bool	operator<=(vector<T, Alloc> const& lhs, vector<T, Alloc> const& rhs){
-		return (!(rhs < lhs));
-	}
-
-	template <class T, class Alloc>
-	bool	operator>(vector<T, Alloc> const& lhs, vector<T, Alloc> const& rhs){
-		return (rhs < lhs);
-	}
-
-	template <class T, class Alloc>
-	bool	operator>=(vector<T, Alloc> const& lhs, vector<T, Alloc> const& rhs){
-		return (!(lhs < rhs));
-	}
-
-		//Non-member swap
+	//Swap the 2 elements
 	template <class T, class Alloc>
 	void	swap(vector<T, Alloc>& x, vector<T, Alloc>& y){
 		vector<T, Alloc> tmp = x;
@@ -503,6 +439,50 @@ namespace ft{
 		y = tmp;
 	}
 
+	/*****************************************************************************/
+	/*                          Comparaisons overloads                           */
+	/*****************************************************************************/
+
+	//Equal operator
+	template <class T, class Alloc>
+	bool	operator==(vector<T, Alloc> const& lhs, vector<T, Alloc> const& rhs){
+		if (lhs.size() == rhs.size())
+			return (ft::equal(lhs.begin(), lhs.end(), rhs.begin()));
+		return (false);
+	}
+
+	//Not equal operator
+	template <class T, class Alloc>
+	bool	operator!=(vector<T, Alloc> const& lhs, vector<T, Alloc> const& rhs){
+		if (!(lhs == rhs))
+			return (true);
+		return (false);
+	}
+
+	//Inferior operator
+	template <class T, class Alloc>
+	bool	operator<(vector<T, Alloc> const& lhs, vector<T, Alloc> const& rhs){
+		return (ft::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end()));
+	}
+
+	//Equal or inferor operator
+	template <class T, class Alloc>
+	bool	operator<=(vector<T, Alloc> const& lhs, vector<T, Alloc> const& rhs){
+		return (!(rhs < lhs));
+	}
+
+	//Superior operator
+	template <class T, class Alloc>
+	bool	operator>(vector<T, Alloc> const& lhs, vector<T, Alloc> const& rhs){
+		return (rhs < lhs);
+	}
+
+	//Equal or superior operator
+	template <class T, class Alloc>
+	bool	operator>=(vector<T, Alloc> const& lhs, vector<T, Alloc> const& rhs){
+		return (!(lhs < rhs));
+	}
+
 }//end of namespace my
 
-#endif
+#endif//End of the header

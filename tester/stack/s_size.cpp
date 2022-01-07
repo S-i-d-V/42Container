8d3ab454932d3959ddf333c50f6c2a631658b675
Ugo Theveny <utheveny42@gmail.com>
2022-01-08 00:41:52 +0100

    test on tester update

--- tester/stack/s_size.cpp
+++ tester/stack/s_size.cpp
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/07 02:43:24 by user42            #+#    #+#             */
-/*   Updated: 2022/01/07 03:09:59 by user42           ###   ########.fr       */
+/*   Updated: 2022/01/07 16:13:40 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,9 +39,79 @@ bool	size_empty_stack(){
 	return false;
 }
 
+bool	size_filled_stack(){
+	std::cout << "\033[0;1mTest 2\033[0m - ";
+	std::stack<int>		rstack;
+	ft::stack<int>		mstack;
+	for (int i = 0; i < 10; i++){
+		rstack.push(i);
+		mstack.push(i);
+	}
+
+	//std test
+	std::time_t time1 = std::time(NULL);
+	int ret1 = rstack.size();
+	time1 = std::time(NULL) - time1;
+
+	//ft test
+	std::time_t time2 = std::time(NULL);
+	int ret2 = mstack.size();
+	time2 = std::time(NULL) - time2;
+
+	//get diff time
+	std::time_t diff = time2 - time1;
+
+	//ret
+	if (ret1 == ret2){
+		std::cout << "\033[32mOK\033[0m - \033[33m" << diff << " ms\033[0m" << std::endl;
+		return true;
+	}
+	std::cout << "\033[31mKO\033[0m" << std::endl;
+	return false;
+}
+
+bool	size_cleared_stack(){
+	std::cout << "\033[0;1mTest 3\033[0m - ";
+	std::stack<int>		rstack;
+	ft::stack<int>		mstack;
+	for (int i = 0; i < 10; i++){
+		rstack.push(i);
+		mstack.push(i);
+	}
+	for (int i = 0; i < 10; i++){
+		rstack.pop();
+		mstack.pop();
+	}
+
+	//std test
+	std::time_t time1 = std::time(NULL);
+	int ret1 = rstack.size();
+	time1 = std::time(NULL) - time1;
+
+	//ft test
+	std::time_t time2 = std::time(NULL);
+	int ret2 = mstack.size();
+	time2 = std::time(NULL) - time2;
+
+	//get diff time
+	std::time_t diff = time2 - time1;
+
+	//ret
+	if (ret1 == ret2){
+		std::cout << "\033[32mOK\033[0m - \033[33m" << diff << " ms\033[0m" << std::endl;
+		return true;
+	}
+	std::cout << "\033[31mKO\033[0m" << std::endl;
+	return false;
+}
+
 bool	stack_size_test(){
 	std::cout << std::endl << "\033[34;1mSize() :\033[0m" << std::endl;
 	if (size_empty_stack() == false)
+		return (print_function_ko());
+	if (size_filled_stack() == false)
+		return (print_function_ko());
+	if (size_cleared_stack() == false)
 		return (print_function_ko());
 	return (print_function_ok());
 }

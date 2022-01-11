d522f5deec0dda7e0acb8b511999288f34bd676d
Ugo Theveny <utheveny42@gmail.com>
2022-01-11 03:17:36 +0100

    adding makefile for 2 binaries

---
+++ binaries/ft.cpp
@@ -0,0 +1,10 @@
+#include "../containers/vector/vector.hpp"
+#include "../containers/stack/stack.hpp"
+#include "../containers/map/map.hpp"
+
+#include <iostream>
+
+int		main(){
+	std::cout << "Testing binarie" << std::endl;
+	return 0;
+}
---
+++ binaries/std.cpp
@@ -0,0 +1,11 @@
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <map>
+
+#include <iostream>
+
+int		main(){
+	std::cout << "Testing binarie" << std::endl;
+	return 0;
+}
